4b12f5dabdec4a0103a8a5cc0e65ef80346402ee
Jansen Cruz <jansencruz12345@gmail.com>
2024-08-21 10:23:47 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Module01Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Firstname { get; set; }
+        public Color ColorRed { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Firstname = "Hello Mark!";
+            ColorRed = Colors.Red;
+
+            this.BindingContext = this;
+
+           
+        }
+
+        private void OnChangedLabelName(object sender, EventArgs e)
+        {
+            this.Resources["StaticUsername"] = "ISPAYDORMAN";
+        }
+
+        private void OnChangedLabelNameDynamic(object sender, EventArgs e)
+        {
+            this.Resources["DynamicUsername"] = "ISPAYDORMAN";
+        }
+
+        private void OnChangedBackgroundColor(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightGray;
+        }
+
+        private void OnChangedBackgroundColorG(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Green;
+        }
+        private void OnChangedBackgroundColorL(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightBlue;
+        }
+
+        private void OnChangedBoxViewColor(object sender, EventArgs e)
+        {
+            this.Resources["DynamicBoxColor"] = Colors.Red;
+        }
+    }
+
+}
